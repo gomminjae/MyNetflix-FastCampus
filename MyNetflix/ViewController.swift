09f863f253e963fffacb5883376dbfb0fea079d7
joonwon lee <cafielo@gmail.com>
2020-04-01 15:14:07 +0900

    Initial Commit

---
+++ MyNetflix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNetflix
+//
+//  Created by joonwon lee on 2020/04/01.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
