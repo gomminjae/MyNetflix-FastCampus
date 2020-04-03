d6e19bd3355bd0002e3e35f510498b2dcc00a10d
joonwon lee <cafielo@gmail.com>
2020-04-03 15:16:00 +0900

    초기 상태

--- MyNetflix/PlayerViewController.swift
+++ MyNetflix/PlayerViewController.swift
@@ -7,66 +7,22 @@
 //
 
 import UIKit
-import AVFoundation
 
 class PlayerViewController: UIViewController {
 
-    @IBOutlet weak var playerView: PlayerView!
-    @IBOutlet weak var controlView: UIView!
-    @IBOutlet weak var playButton: UIButton!
-    
-    let player = AVPlayer()
-    
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .landscapeRight
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        playerView.player = player
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        play()
-    }
 
     @IBAction func closeButtonTapped(_ sender: Any) {
-        reset()
         dismiss(animated: false, completion: nil)
-    }
-    
-    @IBAction func togglePlayButton(_ sender: Any) {
-        if player.isPlaying {
-            pause()
-        } else {
-            play()
-        }
-    }
-    
-    private func play() {
-        player.play()
-        playButton.isSelected = true
-    }
-    
-    private func pause() {
-        player.pause()
-        playButton.isSelected = false
-    }
-    
-    private func reset() {
-        pause()
-        player.replaceCurrentItem(with: nil)
     }
 }
 
-extension AVPlayer {
-    var isPlaying: Bool {
-        guard self.currentItem != nil else { return false }
-        return self.rate != 0
-    }
-}
+//extension AVPlayer {
+//    var isPlaying: Bool {
+//        guard self.currentItem != nil else { return false }
+//        return self.rate != 0
+//    }
+//}
