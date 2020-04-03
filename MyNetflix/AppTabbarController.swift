bc3ea41ddc01437aba273e74ab538a8f8a3000d7
joonwon lee <cafielo@gmail.com>
2020-04-03 14:27:55 +0900

    완성 1탄

--- MyNetflix/AppTabbarController.swift
+++ MyNetflix/AppTabbarController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AppTabbarController: UITabBarController {
+    
+   override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
