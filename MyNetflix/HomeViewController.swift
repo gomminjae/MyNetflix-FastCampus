c486d63d799e1359c07a242a3ed285ecc8a7f640
joonwon lee <cafielo@gmail.com>
2020-04-03 18:32:43 +0900

    9 오늘의 영화 구현 완료

--- MyNetflix/HomeViewController.swift
+++ MyNetflix/HomeViewController.swift
@@ -41,18 +41,24 @@ class HomeViewController: UIViewController {
     
 
     @IBAction func playButtonTapped(_ sender: Any) {
-//        SearchAPI.search("interstella") { movies in
-//            guard let interstella = movies.first else { return }
-//            DispatchQueue.main.async {
-//                let url = URL(string: interstella.previewURL)!
-//                let item = AVPlayerItem(url: url)
-//                
-//                let sb = UIStoryboard(name: "Player", bundle: nil)
-//                let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
-//                vc.modalPresentationStyle = .fullScreen
-//                vc.player.replaceCurrentItem(with: item)
-//                self.present(vc, animated: false, completion: nil)
-//            }
-//        }
+        // - [x] intersetlla 에 대한 정보를 검색API로 가져온다
+        // - [x] 거기서 interstella  객체 하나를 가져온다
+        // - [x] 그 객체를 이용해서 PlayerViewControllr 를 띄운다
+        
+        
+        SearchAPI.search("interstella") { movies in
+            guard let interstella = movies.first else { return }
+            
+            DispatchQueue.main.async {
+                let url = URL(string: interstella.previewURL)!
+                let item = AVPlayerItem(url: url)
+                let sb = UIStoryboard(name: "Player", bundle: nil)
+                let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
+                vc.player.replaceCurrentItem(with: item)
+                
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: false, completion: nil)
+            }
+        }
     }
 }
